f5e71b4918ab72458e6e8f1acf813eb07820b231
toujames <james@touthang.info>
2026-09-13 01:55:13 -0500

    Route the public songbook privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,11 @@ export const routes: Routes = [
     title: 'Songbook | PBC'
   },
   {
+    path: 'songbook/privacy',
+    loadComponent: () => import('./features/songbook/songbook-privacy.component').then((m) => m.SongbookPrivacyComponent),
+    title: 'PBC Songbook Privacy Policy | PBC'
+  },
+  {
     path: 'songbook/:id',
     loadComponent: () => import('./features/songbook/song-detail/song-detail.component').then((m) => m.SongDetailComponent),
     title: 'Song Detail | PBC'
